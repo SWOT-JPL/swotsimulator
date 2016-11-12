11114fe3957e94fda8b904fd7dd564224dbc9a25
Lucile Gaultier <lucile.gaultier@gmail.com>
2016-11-12 11:58:29 +0100

    Update Fig5.py with new karin noise, need to update y range

--- doc/images/code_image/Fig5.py
+++ doc/images/code_image/Fig5.py
@@ -12,13 +12,14 @@ fig = plt.figure(figsize=(12,9))
 tloc=0.11
 tfont=20
 stitle = 'Karin noise standard deviation as a function of distance from nadir'
-for i in range(0, 8):
+for i in range(0, 9):
      swh = i
      karin_data.read_karin(i)
      hsdt = karin_data.hsdt
      x_ac = karin_data.x_ac
      plt.plot(x_ac, hsdt, label = str(int(swh)) + ' m')
      plt.legend()
+#plt.yrange(0,0.25)
 plt.ylabel('Karin std')
 plt.xlabel('across track (km)')
 plt.title(stitle, y=-tloc, fontsize=tfont) #size[1])
